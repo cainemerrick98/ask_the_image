ad3198cfff9cba713219cf00beda1e9c6a5cd9cc
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2025-12-03 03:49:20 +0000

    TODO added to LLM module

--- src/llm.py
+++ src/llm.py
@@ -3,6 +3,9 @@ from prompts import CREATE_SCHEMA, CREATE_QUERY_TO_RESPOND_TO_USER
 from models import Schema, Query
 from database import Database
 
+# TODO: In the init create a conversation object and then the LLM always has all the context from the create schema
+# to the creation of different queries
+# to the answering of user questions
 class LLM():
     def __init__(self, api_key: str):
         self.client = OpenAI(api_key=api_key)
